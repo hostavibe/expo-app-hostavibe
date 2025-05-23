9169540042c4a3a23bc8e6c3db04b8d5ffa8a444
Albert Boehmler <ajboehmler@gmail.com>
2025-05-23 08:57:13 -0400

    try this

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,13 @@ import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Slot } from 'expo-router';
 
+
 export default function RootLayout() {
+  const clerkPublishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+  if (!clerkPublishableKey) {
+    throw new Error('Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY');
+  }
+
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <UserProvider>
