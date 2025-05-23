3d48bbb1fcb5d7116c1027e958b1785f8dc722ab
Albert Boehmler <ajboehmler@gmail.com>
2025-05-23 08:59:57 -0400

    duh

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={clerkPublishableKey}>
       <UserProvider>
         <Slot />
       </UserProvider>
